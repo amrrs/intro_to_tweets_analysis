df8df05cbe4387adbce9d46d585251cbda16912e
amrrs <amrrs@outlook.com>
2019-04-20 09:09:11 +0530

    tweet collection

---
+++ tweet_collection.R
@@ -0,0 +1,21 @@
+library(rtweet)
+#library(tidyverse)
+
+
+consumer_key ="xxxx"
+
+consumer_secret ="xxxx"
+
+access_token="xxxx"
+
+access_secret="xxxx"
+
+
+twitter_token = create_token(consumer_key = consumer_key,
+                             consumer_secret = consumer_secret,
+                             access_token = access_token,
+                             access_secret = access_secret)
+
+keyword1 <- search_tweets('@hasanminhaj india', n = 5000, token = twitter_token, include_rts = FALSE)
+
+write_as_csv(keyword1,"~//Documents//R Codes//hasanminhaj_india_noRT.csv")
